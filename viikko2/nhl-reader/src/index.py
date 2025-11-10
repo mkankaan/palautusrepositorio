e60b2f5cbe5076831fd8ada6f394e3aa6f83dd70
mkankaan <104016919+mkankaan@users.noreply.github.com>
2025-11-10 03:30:50 +0200

    Week 2 ex 5

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,15 +1,55 @@
 from player_reader import PlayerReader
 from player_stats import PlayerStats
+from rich.console import Console
+from rich.table import Table
 
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    reader = PlayerReader(url)
-    stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+    seasons = ""
+    y = 18
+    steps = 8
 
-    for player in players:
-        print(player)
+    for i in range(steps):
+        seasons += f"20{y}-{y+1}"
+
+        if i < steps-1:
+            seasons += "/"
+        y += 1
+    
+    season_list = f"[{seasons}]"
+    default_season = "2024-25"
+    nationalities = "USA/FIN/CAN/SWE/CZE/RUS/SLO/FRA/GBR/SVK/DEN/NED/" \
+        "AUT/BLR/GER/SUI/NOR/UZB/LAT/AUS"
+    
+    console = Console()
+    season = console.input(f"Season [purple bold]{season_list}[/] [blue_violet bold]({default_season})[/]: ")
+
+    if season == "":
+        season = default_season
+
+    while True:
+        nationality = console.input(f"Nationality [purple bold][{nationalities}][/] [blue_violet bold]()[/]: ").upper()
+
+        if nationality == "":
+            continue
+
+        url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+        reader = PlayerReader(url)
+        stats = PlayerStats(reader)
+        players = stats.top_scorers_by_nationality(nationality)
+
+        table = Table(title=f"Season {season} players from {nationality}")
+        table.add_column("Released", style="blue_violet")
+        table.add_column("teams", style="purple")
+        table.add_column("goals", style="cyan")
+        table.add_column("assists", style="cyan")
+        table.add_column("points", style="cyan")
+
+        for player in players:
+            table.add_row(player.name, player.team, str(player.assists),
+                          str(player.goals), str(player.assists + player.goals))
+
+        console.print(table)
 
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player_reader.py
+++ viikko2/nhl-reader/src/player_reader.py
@@ -1,13 +1,13 @@
 import requests
 from player import Player
 
+
 class PlayerReader():
     def __init__(self, url):
         self._url = url
 
     def get_players(self):
-        url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-        response = requests.get(url).json()
+        response = requests.get(self._url).json()
         players = []
 
         for player_dict in response:
