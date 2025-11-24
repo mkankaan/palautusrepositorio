491dd73e406c1ce53023fbce0150554faf4837cf
mkankaan <104016919+mkankaan@users.noreply.github.com>
2025-11-24 21:03:39 +0200

    Week 3 ex 4

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -10,7 +10,7 @@ class TestKauppa(unittest.TestCase):
         self.pankki_mock = Mock()
         self.viitegeneraattori_mock = Mock()
         self.viitegeneraattori_mock.uusi.return_value = 42
-        self.varasto_mock = Mock()
+        self.varasto_mock = Mock(wraps=Varasto())
 
         def varasto_saldo(tuote_id):
             if tuote_id < 3:
@@ -108,3 +108,19 @@ class TestKauppa(unittest.TestCase):
         kauppa.lisaa_koriin(1)
         kauppa.tilimaksu("c", "333")
         self.assertEqual(viitegeneraattori_mock.uusi.call_count, 3)
+
+
+    def test_poistettaessa_tuote_varaston_metodia_hae_tuote_kutsutaan(self):
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.poista_korista(1)
+        self.varasto_mock.hae_tuote.assert_called_with(1)
+
+
+    def test_poistettaessa_tuote_varaston_metodia_palauta_tuote_kutsutaan(self):
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.poista_korista(1)
+        self.varasto_mock.palauta_varastoon.assert_called()
