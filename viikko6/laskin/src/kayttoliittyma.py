9d50d05a5178f18d5af55b2a53909542b7db4ce0
mkankaan <104016919+mkankaan@users.noreply.github.com>
2025-12-08 22:35:51 +0200

    Week 6 ex 1

--- viikko6/laskin/src/kayttoliittyma.py
+++ viikko6/laskin/src/kayttoliittyma.py
@@ -8,11 +8,49 @@ class Komento(Enum):
     NOLLAUS = 3
     KUMOA = 4
 
+class Summa:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        self._sovelluslogiikka.plus(int(self._io()))
+
+class Erotus:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        self._sovelluslogiikka.miinus(int(self._io()))
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+
+class Kumoa:
+    def __init__(self, sovelluslogiikka, io):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._io = io
+
+    def suorita(self):
+        pass
 
 class Kayttoliittyma:
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+
+        self._komennot = {
+            Komento.SUMMA: Summa(sovelluslogiikka, self._lue_syote),
+            Komento.EROTUS: Erotus(sovelluslogiikka, self._lue_syote),
+            Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote),
+            Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote) # ei ehkä tarvita täällä...
+        }
 
     def kaynnista(self):
         self._arvo_var = StringVar()
@@ -54,23 +92,12 @@ class Kayttoliittyma:
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
 
+    def _lue_syote(self):
+        return self._syote_kentta.get()
+
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
         self._kumoa_painike["state"] = constants.NORMAL
 
         if self._sovelluslogiikka.arvo() == 0:
@@ -80,3 +107,4 @@ class Kayttoliittyma:
 
         self._syote_kentta.delete(0, constants.END)
         self._arvo_var.set(self._sovelluslogiikka.arvo())
+
