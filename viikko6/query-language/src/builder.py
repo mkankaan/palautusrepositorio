06934766c0043a13d56aaa3d944694551419843e
mkankaan <104016919+mkankaan@users.noreply.github.com>
2025-12-09 21:25:24 +0200

    Week 6 ex 5

--- viikko6/query-language/src/builder.py
+++ viikko6/query-language/src/builder.py
@@ -6,18 +6,23 @@ class Query:
 
     def build(self):
         return And(*self.matchers)
-    
-    def add_matcher(self, matcher):
-        self.matchers.append(matcher)
 
 class QueryBuilder:
     def __init__(self, query = Query()):
         self.query = query
 
     def build(self):
-        return self.query
+        return self.query.build()
     
     def plays_in(self, team):
-        self.query.add_matcher(PlaysIn(team))
-        return self.query
-
+        self.query.matchers.append(PlaysIn(team))
+        return QueryBuilder(self.query)
+    
+    def has_at_least(self, value, attr):
+        self.query.matchers.append(HasAtLeast(value, attr))
+        return QueryBuilder(self.query)
+    
+    def has_fewer_than(self, value, attr):
+        self.query.matchers.append(HasFewerThan(value, attr))
+        return QueryBuilder(self.query)
+   
--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,26 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All, Or
 from builder import QueryBuilder
-
-"""
-def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
-    reader = PlayerReader(url)
-    stats = Statistics(reader)
-
-    matcher = And(
-        HasAtLeast(70, "points"),
-            Or(
-                PlaysIn("COL"),
-                PlaysIn("FLA"),
-                PlaysIn("BOS")
-            )
-    )
-
-    for player in stats.matches(matcher):
-        print(player)
-"""
 
 def main():
     url = "https://studies.cs.helsinki.fi//nhlstats/2024-25/players.txt"
@@ -29,8 +9,14 @@ def main():
 
     query = QueryBuilder()
 
-    matcher = query.plays_in("NYR").build()
-
+    matcher = (
+        query
+        .plays_in("NYR")
+        .has_at_least(10, "goals")
+        .has_fewer_than(20, "goals")
+        .build()
+    )
+    
     for player in stats.matches(matcher):
         print(player)
 
